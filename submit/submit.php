4b7db314654d1ca05d381711e3b40d937c971df6
Aaron Weidele <aweidele@gmail.com>
2017-09-09 14:25:14 -0400

    Insert form data in DB

--- submit/submit.php
+++ submit/submit.php
@@ -17,14 +17,33 @@
   /*
   INSERT INTO form_submissions TABLE
   */
-  $sql = 'INSERT INTO form_submissions (submission_fname,submission_lname,submission_form_type)
-          VALUES (
-            "'.$_POST['NameFirst'].'",
-            "'.$_POST['NameLast'].'",
-            "'.$_POST['form-type'].'"
+  $sql = 'INSERT INTO form_submissions (submission_fname,submission_lname,submission_form_type) VALUES (
+      "'.$_POST['NameFirst'].'",
+      "'.$_POST['NameLast'].'",
+      "'.$_POST['form-type'].'"
   )';
 
   if ($conn->query($sql) === TRUE) {
+    $submission_id = $conn->insert_id;
+  } else {
+    die("Error: " . $sql . "<br>" . $conn->error);
+  }
+
+  /*
+  INSERT FORM DATA
+  */
+  $sql = '';
+  foreach($_POST as $name => $val) {
+    if($name != 'NameFirst' && $name != 'NameLast' && $name != 'form-type') {
+      $sql .= 'INSERT INTO submission_data (name,value,submission) VALUES (
+        "'.$name.'",
+        "'.$val.'",
+        '.$submission_id.'
+      );';
+    }
+  }
+
+  if ($conn->multi_query($sql) === TRUE) {
     $submission_id = $conn->insert_id;
   } else {
     die("Error: " . $sql . "<br>" . $conn->error);
