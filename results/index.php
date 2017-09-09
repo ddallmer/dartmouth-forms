fd6ed9d9dbae2923094cb801db1d88f0335c3b74
Aaron Weidele <aweidele@gmail.com>
2017-09-09 16:43:31 -0400

    Add results table page

--- results/index.php
+++ results/index.php
@@ -1,27 +1,4 @@
 <?php
 header('Content-Type: application/json');
-require_once('../inc/conn.php');
-
-/*
-SELECT query
-*/
-$sql = "
-  SELECT *
-  FROM form_submissions, submission_data
-  WHERE submission_data.submission = form_submissions.submission_ID
-  ORDER BY submission_form_type, submission_time, submission_data.id";
-$result = $conn->query($sql);
-
-/*
-COMPILE RESULTS
-*/
-$submissions = [];
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    $submissions[$row['submission_ID']]['index'] = $row['submission_form_type'];
-    $submissions[$row['submission_ID']]['name'] = $row['submission_fname'].' '.$row['submission_lname'];
-    $submissions[$row['submission_ID']][$row['name']] = $row['value'];
-  }
-}
-
+require_once('results.php');
 echo json_encode( array_values($submissions) );
